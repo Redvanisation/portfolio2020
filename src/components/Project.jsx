7c180be97832b2a4ea39f5289045abf03a78899b
Redvan <redvanisation@gmail.com>
2020-02-01 21:31:02 -0500

    Add projects' placeholders

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProjectImage from './ProjectImage';
 
 const Project = ({ name, description }) => (
   <div className="projects__project column">
-    <h3 className="title is-4 projects__project--title">{name}</h3>
-    <p className="projects__project--description">{description}</p>
+    {/* <h3 className="title is-4 projects__project--title">{name}</h3>
+    <p className="projects__project--description">{description}</p> */}
+    <ProjectImage url={require('../assets/mac.png')} />
   </div>
 );
 
---
+++ src/components/ProjectImage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProjectImage = ({ url }) => (
+  <div className="projects__project--show">
+    <img src={url} alt="" />
+  </div>
+);
+
+export default ProjectImage;
