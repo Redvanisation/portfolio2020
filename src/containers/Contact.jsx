8ef04fcecd45bee078dd1aababf9b6a973ba1857
Redvan <redvanisation@gmail.com>
2020-02-06 17:13:06 -0500

    Add contact form functionality

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,42 +1,81 @@
-import React from 'react';
-import emailjs from 'emailjs-com';
+import React, { Component } from 'react';
+import * as emailjs from 'emailjs-com';
 
-export default function Contact() {
-  function sendEmail(e) {
-  //   e.preventDefault();
-  
-  //   emailjs.sendForm('portfolio_email', 'contact_form', e.target, 'user_J7qkTKmKiuAZN1jhMiF8o')
-  //     .then((result) => {
-  //         console.log(result.text);
-  //     }, (error) => {
-  //         console.log(error.text);
-  //     });
+
+class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  return (
-    <section className="contact has-text-centered">
-    <h2 className="contact__title title is-2">Contact</h2>
-      
-      <form className="contact-form" onSubmit={sendEmail}  className="container contact__form">
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const { name, email, message } = this.state;
+
+    const tempParams = {
+      from_name: name,
+      from_email: email,
+      to_name: 'Radouane Khiri',
+      message_html: message,
+    };
+
+    emailjs.send(
+      'portfolio_email',
+      'contact_form',
+      tempParams,
+      'user_J7qkTKmKiuAZN1jhMiF8o',
+    );
+
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.setState({
+      name: '',
+      email: '',
+      message: '',
+    });
+  }
+
+  handleChange(param, e) {
+    this.setState({ [param]: e.target.value });
+  }
+
+  render() {
+    return (
+      <section className="contact has-text-centered">
+        <h2 className="contact__title title is-2">Contact</h2>
         
-        <input type="hidden" name="contact_number" />
-        
-        <div className="field">
-          <div className="control">
-            <input className="input contact__form-inputs" type="text" name="user_name" placeholder="Name" />
+        <form onSubmit={this.handleSubmit.bind(this)} className="container contact__form">
+          
+          <input type="hidden" name="contact_number" />
+          
+          <div className="field">
+            <div className="control">
+              <input className="input contact__form-inputs" type="text" name="name" value={this.state.name} onChange={this.handleChange.bind(this, 'name')} placeholder="Name" />
+            </div>
           </div>
-        </div>
-
-        <div className="field">
-          <div className="control">
-            <input className="input contact__form-inputs" type="email" name="user_email" placeholder="Email" />
+  
+          <div className="field">
+            <div className="control">
+              <input className="input contact__form-inputs" type="email" name="email" value={this.state.email} onChange={this.handleChange.bind(this, 'email')} placeholder="Email" />
+            </div>
           </div>
-        </div>
-
-        <textarea className="textarea contact__form-inputs" placeholder="Message" rows="10" />
-
-        <input type="submit" value="Send" className="button" />
-      </form>
-    </section>
-  );
+  
+          <textarea className="textarea contact__form-inputs" value={this.state.message} onChange={this.handleChange.bind(this, 'message')} placeholder="Message" rows="10" />
+  
+          <input type="submit" value="Send" className="button" />
+        </form>
+      </section>
+    );
+  }
 }
+
+
+export default Contact;
