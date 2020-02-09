97238a351294a9af5a10dd07d4a6a4caf7f2597b
Redvan <redvanisation@gmail.com>
2020-02-08 22:09:30 -0500

    Fix projects section's responsiveness

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -9,7 +9,13 @@ const part2 = theProjects.slice(2, theProjects.length);
 const Projects = () => (
   <section className="projects block has-text-centered">
     <h2 className="section-title projects__title title is-2">Featured Projects</h2>
-    <h3 className="projects__quote title is-4">I didn&apos;t invent the internet I just make it look good!</h3>
+    <h3 className="projects__quote title is-4">
+      <span className="projects__quote--line-1">I didn&apos;t invent the internet </span> 
+      <span className="projects__quote--line-2">
+        I just make it
+        <span className="projects__quote--span"> look good</span>
+      </span>
+    </h3>
     
     <div className="projects__button-div">
       <a href="#" className="button button__resume is-full">Resume</a>
