a1c3fb52655a0531cf6d56c29c026e4d5d404e9d
Redvan <redvanisation@gmail.com>
2020-02-09 09:20:37 -0500

    Add Navbar closing on click feature

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Nav = () => (
-  <div className="navigation">
-    <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
-    <label htmlFor="navi-toggle" className="navigation__button">
-        <span className="navigation__icon">&nbsp;</span>
-    </label>
+class Nav extends Component {
+    constructor() {
+        super();
+        this.state = {
+            checked: false,
+        }
+        this.showHideMenu = this.showHideMenu.bind(this);
+    }
 
-    <div className="navigation__background">&nbsp;</div>
+    showHideMenu() {
+        this.setState({
+            checked: !this.state.checked,
+        });
+    }
 
-    <nav className="navigation__nav">
-        <ul className="navigation__list">
-            <li className="navigation__item">
-                <a href="#home" className="navigation__link">Home</a>
-            </li>
-            <li className="navigation__item">                    
-                <a href="#projects" className="navigation__link">Projects</a>
-            </li>
-            <li className="navigation__item">
-                    <a href="#contact" className="navigation__link">Contact</a>
-            </li>
-            <li className="navigation__item">
-                    <a href="#activity" className="navigation__link">Activity</a>
-            </li>
-        </ul>
-    </nav>
-  </div>
-);
+    render() {
+        const { checked } = this.state;
+        return (
+            <div className="navigation">
+              <input type="checkbox" className="navigation__checkbox" id="navi-toggle" onClick={this.showHideMenu} checked={checked ? true : false} />
+              <label htmlFor="navi-toggle" className="navigation__button">
+                  <span className="navigation__icon">&nbsp;</span>
+              </label>
+          
+              <div className="navigation__background">&nbsp;</div>
+          
+              <nav className="navigation__nav" onClick={this.showHideMenu}>
+                  <ul className="navigation__list">
+                      <li className="navigation__item">
+                          <a href="#home" className="navigation__link" onClick={this,this.showHideMenu}>Home</a>
+                      </li>
+                      <li className="navigation__item">                    
+                          <a href="#projects" className="navigation__link" onClick={this,this.showHideMenu}>Projects</a>
+                      </li>
+                      <li className="navigation__item">
+                              <a href="#contact" className="navigation__link" onClick={this,this.showHideMenu}>Contact</a>
+                      </li>
+                      <li className="navigation__item">
+                              <a href="#activity" className="navigation__link" onClick={this,this.showHideMenu}>Activity</a>
+                      </li>
+                  </ul>
+              </nav>
+            </div>
+          );
+          
+    }
+} 
+
 
 export default Nav;
