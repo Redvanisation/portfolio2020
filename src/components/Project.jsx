8b20a3b5283bfd6c94b4a67819586a1ae388cac0
Redvan <redvanisation@gmail.com>
2020-02-21 20:23:30 -0500

    Update meta tags and projects' descriptions

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -11,8 +11,8 @@ const Project = ({
         <h3 className="title projects__project--title text-colored header-title">{name}</h3>
         <p className="projects__project--description">{description}</p>
         <div className="projects__project--links">
-          <a href={live} className="title is-6 projects__project--link">Demo</a>
-          <a href={github} className="title is-6 projects__project--link">GitHub</a>
+          <a href={live} className="title is-6 projects__project--link" target="_blank" rel="noopener noreferrer">Demo</a>
+          <a href={github} className="title is-6 projects__project--link" target="_blank" rel="noopener noreferrer">GitHub</a>
         </div>
       </figcaption>
     </figure>
