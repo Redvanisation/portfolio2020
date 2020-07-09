22eb599538f4dd9ba45012db823f45a1fd57fc4f
Redvan <redvanisation@gmail.com>
2020-07-08 22:15:32 -0400

    Update about section

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,24 +1,42 @@
 /* eslint-disable max-len */
 import React from 'react';
+import { Link } from 'react-scroll';
 
 const About = () => (
   <section className="about block has-text-centered">
     <h2 className="section-title title is-2" id="about">Who am I</h2>
     <div className="about__content">
-      <p className="about__content--parag contact__content-div--parag">
-        I&apos;m Radouane, a web developer based in New York City.
-        I&apos;m creative, analytical and very passionate about web development.
-        I was previously a lawyer for over 5 years but my passion for web development made me choose a different path.
-        Through my time in web development I acquired professional-level skills including
-        {' '}
-        <span className="heighlight">Ruby, Ruby on Rails, JavaScript, React, Redux, Sass, Test-Driven development techniques</span>
-        {' '}
-        and more. I designed and developed more than 40 professional projects through pair programming with different coding partners remotely from around the world and mentored students on their different projects.
+
+      <p className="about__content--parag">
+        Any programmer worth their salt can make something&nbsp;
+        <span className="heighlight">&quot;work&quot;</span>
+        , but very few can make it look truly incredible.
+      </p>
+      <p className="about__content--parag">
+        I&apos;m Radouane, a full-stack web developer and an ex-lawyer based in New Jersey, USA, I&apos;m a polyglot (Multi-lingual) developer with a great knowledge of&nbsp;
+        <span className="heighlight">software development methodologies</span>
+        &nbsp;,&nbsp;
+        <span className="heighlight">frameworks</span>
+        &nbsp;, and&nbsp;
+        <span className="heighlight">Software Development Life Cycle (SDLC)</span>
+        &nbsp;and I&apos;ve been a team player in multiple development and software consulting projects.
+      </p>
+      <p className="about__content--parag">
+        I&apos;m very passionate about new technologies and I&apos;m constantly learning useful patterns that would help be more throught my path.
+      </p>
+      <p className="about__content--parag about__content--action-call">
+        You have an interesting idea or a project you want to discuss?&nbsp;
+        <br />
+        <Link
+          className="button button__red heighlight about__content--button mt-5"
+          to="contact"
+          smooth
+          duration={500}
+        >
+          CONTACT ME !
+        </Link>
       </p>
 
-      <p className="about__content--parag contact__content-div--parag">
-        I&apos;m a maker at heart and in my free time I like to acquire as many skills as possible, whether it&apos;s learning a new programming language, learning musical instruments, cooking or any other skill I can get the tools for. Both at work and in my personal life, I&apos;m constantly chasing the satisfaction of a tangible finished product. When I&apos;m not working or crafting, I also try to spend time giving back to the community, especially supporting new developers and contributing to apps that can help the dev community over the world.
-      </p>
     </div>
   </section>
 );
