fd0fdae36b0e653289c75afa11880dd1b3c5fc17
Rad <rad@Mac.lan>
2025-03-06 00:32:36 -0500

    Update icons package and Twitter icon

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,27 +1,50 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import {
-  FaGithub, FaTwitter, FaLinkedinIn, FaDev,
-} from 'react-icons/fa';
+import React from "react";
+import { FaGithub, FaTwitter, FaLinkedinIn, FaDev } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Social = () => (
   <div className="social">
-    <a aria-label="Github" href="https://github.com/Redvanisation" target="_blank" rel="noreferrer noopener" className="social__item--link social__item">
+    <a
+      aria-label="Github"
+      href="https://github.com/Redvanisation"
+      target="_blank"
+      rel="noreferrer noopener"
+      className="social__item--link social__item"
+    >
       <div className="social__item">
         <FaGithub className="social__item--icon" />
       </div>
     </a>
-    <a aria-label="Twitter" href="https://twitter.com/redvanisation" target="_blank" rel="noreferrer noopener" className="social__item--link">
+    <a
+      aria-label="Twitter"
+      href="https://twitter.com/redvanisation"
+      target="_blank"
+      rel="noreferrer noopener"
+      className="social__item--link"
+    >
       <div className="social__item">
-        <FaTwitter className="social__item--icon" />
+        <FaXTwitter className="social__item--icon" />
       </div>
     </a>
-    <a aria-label="LinkedIn" href="https://www.linkedin.com/in/redvan/" target="_blank" rel="noreferrer noopener" className="social__item--link">
+    <a
+      aria-label="LinkedIn"
+      href="https://www.linkedin.com/in/redvan/"
+      target="_blank"
+      rel="noreferrer noopener"
+      className="social__item--link"
+    >
       <div className="social__item">
         <FaLinkedinIn className="social__item--icon" />
       </div>
     </a>
-    <a aria-label="Dev.to" href="https://dev.to/redvanisation" target="_blank" rel="noreferrer noopener" className="social__item--link">
+    <a
+      aria-label="Dev.to"
+      href="https://dev.to/redvanisation"
+      target="_blank"
+      rel="noreferrer noopener"
+      className="social__item--link"
+    >
       <div className="social__item">
         <FaDev className="social__item--icon" />
       </div>
