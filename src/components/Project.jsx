95185ca635917c03e609ef5a3beae6ff97f31056
Rad <rad@Mac.lan>
2025-03-06 00:14:01 -0500

    Update dependencies, scripts and add icon

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,19 +1,27 @@
 /* eslint-disable global-require */
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
-const Project = ({
-  name, description, live, github, image,
-}) => (
+const Project = ({ name, description, live, github, image }) => (
   <div className="projects__project column is-one-third">
     <figure className="projects__project--card">
       <img src={image} alt="" className="projects__project--img" />
       <figcaption className="projects__project--description-div">
-        <h3 className="title projects__project--title text-colored header-title">{name}</h3>
+        <h3 className="title projects__project--title text-colored header-title">
+          {name}
+        </h3>
         <p className="projects__project--description">{description}</p>
         <div className="projects__project--links">
-          <a href={live} className="title is-6 projects__project--link" target="_blank" rel="noopener noreferrer">Demo</a>
-          <a href={github} className="title is-6 projects__project--link" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a
+            href={github}
+            className="title is-6 projects__project--link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+            <FaExternalLinkAlt className="projects__project--link-icon" />
+          </a>
         </div>
       </figcaption>
     </figure>
